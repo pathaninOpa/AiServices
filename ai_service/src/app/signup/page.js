4c9d8cc2fa57fba34b14b4155c1e2939e4c155ca
許裕華 <act23541@gmail.com>
2024-07-14 21:44:18 +0700

    Merge Dev into signup

---
+++ ai_service/src/app/signup/page.js
@@ -0,0 +1,25 @@
+"use client"
+import React, {useState} from 'react'
+import Navbar from '../components/Navbar'
+import Link from 'next/link'
+
+function LoginPage() {
+  return (
+    <div>
+      <Navbar />
+      <div className ="container mx-auto py-5">
+        <h3>Sign-up Page</h3>
+        <hr className=" my-3" />
+        <form action="">
+            <input className="block bg-gray-300 p-2 my-2 rounded-md"type="email" placeholder="Enter your email" />
+            <input className="block bg-gray-300 p-2 my-2 rounded-md"type="password" placeholder="Enter your password" />
+            <button type='submit' className='bg-green-500 p-2 rounded-md'>Signup</button>
+        </form>
+        <hr className='my-3' />
+          <p>Does not have an account? <Link className='text-blue-500 hover:underline' href="/register">Sign-up</Link></p>
+      </div>
+    </div>
+  )
+}
+
+export default LoginPage
