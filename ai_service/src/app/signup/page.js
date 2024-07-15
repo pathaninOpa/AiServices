39f47324f4519aab008ba3bc44c363cc8e11df6e
pathaninOpa <pathanin.tae@gmail.com>
2024-07-15 16:16:29 +0700

    Added Documentation.md and update sign-up page with api req and error handling

--- ai_service/src/app/signup/page.js
+++ ai_service/src/app/signup/page.js
@@ -2,21 +2,66 @@
 import React, {useState} from 'react'
 import Navbar from '../components/Navbar'
 import Link from 'next/link'
+import axios from 'axios'
 
 function SignupPage() {
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [confirmPassword, setConfirmPassword] = useState("")
+  const [err, setError] = useState("")
+
+  const  handleSubmit = async (e) => {
+    e.preventDefault();
+    try{
+      if (!name || !email || !password || !confirmPassword){
+        setError("Please Fill Out All The Forms");
+        return;
+      }
+      if (password != confirmPassword){
+        setError("Password Do Not Match!!");
+        return;
+      }
+      const res = await axios.post("https://localhost:8000/api/users",{ // this will be change when we have hosting service (when we deploy)
+        'name':name,
+        'email':email,
+        'password':password
+      });
+      if (res.status === 200){
+        const form = e.target;
+        setError("");
+        form.reset();
+        //localStorage.setItem('name',name), // save variable in local storage for futher usage. ** To do: optimise this ** _Tae Note: Im not sure if this is common practice
+        //window.location.href = '/'; // save to the desired page, modify to change destination
+      } else{
+        console.log("User registration failed.");
+      }
+    }catch(err){
+      console.log("Error During Registration: ",err)
+    }
+  }
   return (
     <div>
       <Navbar />
       <div className ="container mx-auto py-5">
         <h3>Sign-up Page</h3>
         <hr className=" my-3" />
-        <form action="">
-            <input className="block bg-gray-300 p-2 my-2 rounded-md"type="email" placeholder="Enter your email" />
-            <input className="block bg-gray-300 p-2 my-2 rounded-md"type="password" placeholder="Create a password" />
-            <button type='submit' className='bg-green-500 p-2 rounded-md'>Signup</button>
+        <form onSubmit={handleSubmit}>
+          {
+            err && (
+              <div className='bg-red-500 w-fit text-sm text-white py-1 px-3 rounded-md mt-2'>
+                {err}
+              </div>
+            )
+          }
+          <input onChange={(e) => setEmail(e.target.value)} className="block bg-gray-300 p-2 my-2 rounded-md"type="email" placeholder="Enter your email" />
+          <input onChange={(e) => setName(e.target.value)} className="block bg-gray-300 p-2 my-2 rounded-md"type="text" placeholder="Enter your name" />
+          <input onChange={(e) => setPassword(e.target.value)} className="block bg-gray-300 p-2 my-2 rounded-md"type="password" placeholder="Enter your password" />
+          <input onChange={(e) => setConfirmPassword(e.target.value)} className="block bg-gray-300 p-2 my-2 rounded-md"type="password" placeholder="Confirm your password" />
+          <button type='submit' className='bg-green-500 p-2 text-gray-50 rounded-md'>Sign up</button>
         </form>
         <hr className='my-3' />
-          <p>Already signed up? <Link className='text-blue-500 hover:underline' href="/login">Log-in</Link></p>
+          <p>Already signed up? <Link className='text-blue-500 hover:underline' href="/login">Sign-in</Link></p>
       </div>
     </div>
   )
