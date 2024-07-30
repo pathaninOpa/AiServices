039d9557a99f2ed4b63457e3ade2c2c20cc18e5e
Tung <krittapop.kms@gmail.com>
2024-07-30 20:56:09 +0700

    change env variable name

--- ai_service/lib/mongodb.js
+++ ai_service/lib/mongodb.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 export const connectMongoDB = async () =>{
     try{
-        await mongoose.connect(process.env.MONGODB_URI)
+        await mongoose.connect(process.env.uri)
         console.log("Connected to MongoDB")
     }
     catch(error){
--- ai_service/server/database.js
+++ ai_service/server/database.js
@@ -5,11 +5,9 @@ const mongoose = require('mongoose');
 const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
 
 async function run() {
-  console.log("BBB")
-  console.log(process.env.MONGODB_URI)
   try {
     // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
-    await mongoose.connect(process.env.MONGODB_URI, clientOptions);
+    await mongoose.connect(process.env.uri, clientOptions);
     await mongoose.connection.db.admin().command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
